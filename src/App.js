3bad3e4f4fbbd6075d3c9687ebb487033c27a53d
agohorel <gohorelalex@gmail.com>
2020-01-20 13:04:02 -0500

    add cart persistance w/ localStorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 import { ProductContext } from "./contexts/ProductContext";
 import { CartContext } from "./contexts/CartContext";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -12,6 +13,7 @@ import ShoppingCart from "./components/ShoppingCart";
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
+  const [localStorage, setLocalStorage] = useLocalStorage("shoppping_cart", []);
 
   const addItem = item => {
     setCart(prev => [...prev, item]);
@@ -20,6 +22,14 @@ function App() {
   const removeItem = item => {
     setCart(prev => prev.filter(product => product.id !== item.id));
   };
+
+  useEffect(() => {
+    setLocalStorage(cart);
+  }, [cart]);
+
+  useEffect(() => {
+    setCart(localStorage);
+  }, []);
 
   return (
     <div className="App">
